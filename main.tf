a42a575996c29e3150e928d5c0f5d02361e260bd
imlinux68 <imlinux68@gmail.com>
2023-09-16 12:50:20 +0300

    added vpc section

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ terraform {
 provider "aws" {
   region = "us-west-1"
 }
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "Main VPC"
+  }
+}
